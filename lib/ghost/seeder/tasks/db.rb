bd673ef05704e2c872e31d7b7f3b55e3384d2084
Mario Andrés Correa <a@macool.me>
2016-10-20 18:58:41 -0500

    testing rake task

---
+++ lib/ghost/seeder/tasks/db.rb
@@ -0,0 +1,20 @@
+module Ghost
+  module Seeder
+    module Tasks
+      class Db
+        include Rake::DSL if defined? Rake::DSL
+
+        def load_tasks
+          namespace :db do
+            desc "set FORCE=true if you want to wipe"
+            namespace :seed do
+              puts "seeding!"
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+Ghost::Seeder::Tasks::Db.new.load_tasks
